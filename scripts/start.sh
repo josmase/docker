c837f6b92280204af0c31c01c9adb9ca797678a6
Jonas Lundberg <josnase@gmail.com>
2021-01-24 00:56:28 +0000

    Remove volumes after starting

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,3 +5,4 @@ echo "$file_command"
 eval "docker-compose ${file_command} up -d --remove-orphans | tee -a $logdir/start.log"
 
 docker image prune -fa | tee -a $logdir/prune.log
+docker volume prune -f | tee -a $logdir/prune.log
