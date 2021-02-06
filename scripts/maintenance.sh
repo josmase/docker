5153f61e5830e533d1fbf072da6983931ef57949
Jonas Lundberg <josnase@gmail.com>
2021-02-06 00:14:05 +0000

    Add change of owner to appdata and storage before starting after maintenance

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -4,4 +4,5 @@ source update.sh
 source down.sh
 rsync -aP --delete ../appdata /mnt/storage/backups
 snapraid -c ../snapraid.conf sync
+chown -R jonas:jonas ../appdata /mnt/storage/
 source start.sh
