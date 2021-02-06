b6445108ea3a5dd7e74106e34e7c6f63cc0a2d9c
Jonas Lundberg <josnase@gmail.com>
2021-02-06 00:50:33 +0000

    Add log cleanup

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo =====================  $(date)  ==============================================
 source update.sh
 source down.sh
+source cleanup.sh
 rsync -aP --delete ../appdata /mnt/storage/backups
 snapraid -c ../snapraid.conf sync
-chown -R jonas:jonas ../appdata /mnt/storage/
+chown -R jonas:jonas ../appdata /mnt/storage
 source start.sh
