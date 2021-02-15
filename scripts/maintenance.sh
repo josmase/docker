778c5a950f386df3140f56bc6b187bb68fac0c33
Jonas Lundberg <josnase@gmail.com>
2021-02-15 21:20:11 +0000

    Don't change permissions and owner of files

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -5,5 +5,6 @@ source down.sh
 source cleanup.sh
 rsync -aP --delete ../appdata /mnt/storage/backups
 snapraid -c ../snapraid.conf sync
-chown -R jonas:jonas ../appdata /mnt/storage
+#chown -R jonas:jonas ../ /mnt/storage
+#chown -R 774 ../ /mnt/storage
 source start.sh
